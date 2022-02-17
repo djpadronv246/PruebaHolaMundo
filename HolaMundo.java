b845a69973c38636db3d13df9a690801004bcf6f
djpadronv246 <99739279+djpadronv246@users.noreply.github.com>
2022-02-17 08:06:52 -0600

    Create HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,17 @@
+
+package holamundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Dulce Padron
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
+    }
+    
+}
